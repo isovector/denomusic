0a47a2c09303b066d68f12628def34e56a942043
Sandy Maguire <sandy@sandymaguire.me>
2025-11-04 14:41:29 -0800

    Cleanup etude16

--- src/Etude16.hs
+++ src/Etude16.hs
@@ -1,77 +1,34 @@
-{-# LANGUAGE DerivingVia     #-}
 {-# LANGUAGE LambdaCase      #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeFamilies    #-}
 
 module Etude16 where
 
 import Data.Semigroup
-import Data.Ord
-import Debug.Trace
-import Data.Function
-import Data.List (maximumBy, minimumBy)
-import Control.Monad
-import Data.Maybe
 import Data.Ratio
-import Control.Applicative
-import Rhythm
 import Euterpea (Pitch, PitchClass(..))
-import Euterpea qualified as E
 import Tile
-import Data.Functor.Foldable
-import Data.Functor.Foldable.TH
 
-
-
-bar :: Rhythm a -> Tile a
-bar = asBars 1
-
-
-asBars :: Rational -> Rhythm a -> Tile a
-asBars r
-  = (%% delay r)
-  . foldr fork mempty
-  . fmap (\(i, a) -> delay (getOffset i * r) <> tile (getDuration i * r) a)
-  . intervals
-
-bassRhythm :: Rhythm a -> Rhythm a -> Rhythm a
-bassRhythm a b = tuplet
+bassRhythm :: Tile a -> Tile a -> Tile a
+bassRhythm a b = mconcat
   [ a, b, b
   , a, b, a, b
   ]
 
-bassTemplate :: Pitch -> Pitch -> Pitch -> Rhythm Pitch
+chord :: [a] -> Tile a
+chord = simul . fmap (tile 1)
+
+bassTemplate :: Pitch -> Pitch -> Pitch -> Tile Pitch
 bassTemplate a b c =
-  bassRhythm
-    (chord [a, b])
-    (pure c)
+  scale (1 % 7) $
+    bassRhythm
+      (chord [a, b])
+      (tile 1 c)
 
-asLastBeat :: Rhythm a -> Rhythm a
-asLastBeat a = weightedTuplet
-  [ (5 % 7, empty)
-  , (2 % 7, a)
-  ]
+im :: [a] -> Tile a
+im = foldMap (tile 1)
 
-
-twiddleStrait :: Tile Pitch
-twiddleStrait = do
-  let z = asBars (1 % 4) $
-            im
-              [ (G, 2)
-              , (A, 2)
-              , (G, 2)
-              , (Fs, 2)
-              ]
-  mconcat
-    [ tile 1 (G, 4)
-    , striking (1 % 4) z
-    , co z
-    , tile 1 (G, 4)
-    ]
-
-twiddle :: Tile Pitch
-twiddle =
-  co $ asBars (2 % 7) $
+bassTwiddle :: Tile Pitch
+bassTwiddle =
+  scale (2 % 7) $ scale (1 % 4) $
       im
         [ (G, 2)
         , (A, 2)
@@ -79,117 +36,152 @@ twiddle =
         , (Fs, 2)
         ]
 
-twiddleG :: Tile Pitch
-twiddleG = mconcat
-  [ twiddle
-  , asBars (10 % 7) $ pure (G, 2)
+bassTwiddleG :: Tile Pitch
+bassTwiddleG =
+  co bassTwiddle <> tile (10 % 7) (G, 2)
+
+octaveChord :: PitchClass -> Int -> Tile Pitch
+octaveChord pc o =
+  chord
+      [ (pc, o)
+      , (pc, o + 1)
+      ]
+
+strike1 :: Tile Pitch
+strike1 = mconcat
+  [ co (scale (1 % 7) $ octaveChord Fs 5)
+  , scale (10 % 7) $ octaveChord G 5
   ]
 
-
-octaves :: [Int] -> Pitch -> Tile Pitch
-octaves os (pc, o) = simul  $ fmap (tile 1 . (pc, ) . (+ o)) os
-
-hammer :: Ord b => (Int -> b) -> Tile Pitch -> Tile Pitch
-hammer f t = fromMaybe mempty $ do
-  (Events x y, _) <- uncons t
-  guard $ y == 0
-  let top = maximumBy (on compare $ f . E.absPitch . snd) x
-  pure $ octaves [2, 3] $ snd top
-
-approachU :: Tile Pitch -> Tile Pitch
-approachU = fmap (E.pitch . subtract 1 . E.absPitch)
-
-approachD :: Tile Pitch -> Tile Pitch
-approachD = fmap (E.pitch . (+ 1) . E.absPitch)
-
-striking :: Rational -> Tile Pitch -> Tile Pitch
-striking r t =
-  let ham = hammer id t
-   in co (scale r $ approachU ham) <> ham
-
-strikingD :: Rational -> Tile Pitch -> Tile Pitch
-strikingD r t =
-  let ham = hammer Down t
-   in co (scale r $ approachD ham) <> ham
-
-
-strike :: PitchClass -> PitchClass -> Tile Pitch
-strike pc1 pc2 = mconcat
-  [ co $ asBars (1 % 7) $ chord
-      [ (pc1, 5)
-      , (pc1, 6)
-      ]
-  , asBars (10 % 7) $ chord
-      [ (pc2, 5)
-      , (pc2, 6)
-      ]
+strike2 :: Tile Pitch
+strike2 = mconcat
+  [ co (scale (1 % 7) $ octaveChord Ef 5)
+  , octaveChord D 5
   ]
 
--- parL :: Tile a -> Tile a -> Tile a
--- parL a b = _
+bass1, bass1Twice :: Tile Pitch
+bass1 = bassTemplate (D, 3) (G, 3) (Bf, 3)
+bass1Twice = stimes 2 bass1
 
-b = bar $ bassTemplate (D, 3) (G, 3) (Bf, 3)
+sec1 :: Tile Pitch
+sec1 = mconcat
+  [ bass1Twice
+  , fork bassTwiddleG bass1Twice
+  ]
+
+sec2 :: Tile Pitch
+sec2 = mconcat
+  [ fork strike1 bass1Twice
+  , fork bassTwiddleG bass1Twice
+  ]
+
+secBuilder :: (Int -> Tile Pitch) -> Tile Pitch
+secBuilder f = foldMap (\(i, t) -> fork (f i) t) $
+  zip [0..]
+    [ bass1
+    , bassTemplate (F, 3) (G, 3) (Bf, 3)
+    , bassTemplate (Ef, 3) (G, 3) (Bf, 3)
+    , bassTemplate (Ef, 3) (G, 3) (A, 3)
+    , bassTemplate (C, 3) (F, 3) (A, 3)
+    , bassTemplate (D, 3) (F, 3) (A, 3)
+    , bassTemplate (Ds, 3) (Fs, 3) (A, 3)
+    , bassTemplate (D, 3) (Fs, 3) (A, 3)
+    ]
+
+trebleTwiddle1 :: Tile Pitch
+trebleTwiddle1 =
+  scale (2 % 7) $ scale (1 % 4) $
+      im
+        [ (D, 5)
+        , (Ef, 5)
+        , (D, 5)
+        , (Cs, 5)
+        ]
+
+trebleTwiddle2 :: Tile Pitch
+trebleTwiddle2 =
+  scale (2 % 7) $ scale (1 % 4) $
+      im
+        [ (D, 5)
+        , (Ef, 5)
+        , (D, 5)
+        , (C, 5)
+        ]
+
+
+sec3 :: Tile Pitch
+sec3 = secBuilder $ \case
+  0 -> strike2
+  4 -> co trebleTwiddle1 <> octaveChord D 5
+  _ -> mempty
+
+sec3'2 :: Tile Pitch
+sec3'2 = secBuilder $ \case
+  0 -> co trebleTwiddle1 <> octaveChord D 5
+  4 -> co trebleTwiddle1 <> octaveChord D 5
+  _ -> mempty
+
+ascTriplets :: (Pitch, Pitch) -> (Pitch, Pitch) -> (Pitch, Pitch) -> Tile Pitch
+ascTriplets (l1, h1) (l2, h2) (l3, h3) =
+  mconcat
+    [ co $ scale (4 % 7) $ scale (1 % 2) $ mconcat
+      [ chord [l1, h1]
+      , chord [l2, h2]
+      ]
+    , chord [l3, h3]
+    ]
+
+triple1, triple3, triple5, triple7 :: Tile Pitch
+triple1 = ascTriplets ((Ef, 4), (G, 4)) ((F, 4), (A, 4)) ((G, 4), (Bf, 4))
+triple3 = ascTriplets ((G, 4), (Bf, 4)) ((A, 4), (C, 5)) ((Bf, 4), (D, 5))
+triple5 = ascTriplets ((D, 4), (F, 4)) ((Ef, 4), (G, 4)) ((F, 4), (A, 4))
+triple7 = ascTriplets ((Bf, 3), (D, 4)) ((C, 4), (E, 4)) ((D, 4), (Fs, 4))
+
+sec5 :: Tile Pitch
+sec5 = secBuilder $ \case
+  0 -> co trebleTwiddle2 <> octaveChord Bf 4
+  1 -> triple1
+  3 -> triple3
+  5 -> triple5
+  7 -> triple7
+  _ -> mempty
+
+
+strike3 :: Tile Pitch
+strike3 = mconcat
+  [ co (scale (1 % 7) $ octaveChord D 5)
+  , octaveChord Ef 5
+  ]
+
+strike4 :: Tile Pitch
+strike4 = mconcat
+  [ co (scale (1 % 7) $ octaveChord Ef 5)
+  , octaveChord F 5
+  ]
+
+sec7 :: Tile Pitch
+sec7 = secBuilder $ \case
+  0 -> co trebleTwiddle2 <> octaveChord Bf 4
+  1 -> triple1
+  2 -> strike2
+  3 -> triple3
+  4 -> strike3
+  5 -> triple5
+  6 -> strike4
+  7 -> triple7
+  _ -> mempty
 
 score :: Tile Pitch
-score = mconcat
-  -- [ b
-  -- , b
-  -- , fork twiddleG b
-  -- , b
-  -- , fork (strike Fs G) b
-  -- , b
-  -- , fork twiddleG b
-  [ b2
-  , fork twiddleG b2
-  , fork (striking (1 / 7) b) b2
-  , fork twiddleG b2
-  , fork (strikingD (1 / 7) b) b
-  , b'
-  , b''
-  , b'''
-  -- , fork (fmap (E.trans 24) $ twiddle) b''''
-  -- , b'''''
-  -- , b''''''
-  -- , b'''''''
+score = scale (2 / 3) $ mconcat
+  [ sec1
+  , sec2
+  , sec3
+  , sec3'2
+  , stimes 2 sec5
+  , stimes 2 sec7
   ]
-  where
-    b2 = stimes 2 b
-    b' = bar $ bassTemplate
-          (F, 3)
-          (G, 3)
-          (Bf, 3)
-    b'' = bar $ bassTemplate
-          (Ef, 3)
-          (G, 3)
-          (Bf, 3)
-    b''' = bar $ bassTemplate
-          (Ef, 3)
-          (G, 3)
-          (A, 3)
-    b'''' = bar $ bassTemplate
-          (C, 3)
-          (F, 3)
-          (A, 3)
-    b''''' =
-      bar $ bassTemplate
-          (D, 3)
-          (F, 3)
-          (A, 3)
-    b'''''' =
-      bar $ bassTemplate
-          (Ds, 3)
-          (Fs, 3)
-          (A, 3)
-    b''''''' =
-      bar $ bassTemplate
-          (D, 3)
-          (Fs, 3)
-          (A, 3)
-
 
 main :: IO ()
 main = playTile score
-
-
 
 
