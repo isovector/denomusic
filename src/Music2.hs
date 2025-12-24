f283c0631db595023f5198436dab0d9aabe7af53
Sandy Maguire <sandy@sandymaguire.me>
2025-12-23 22:48:42 -0800

    Better constructor names

--- src/Music2.hs
+++ src/Music2.hs
@@ -12,18 +12,18 @@ import Data.Function.Step.Discrete.Open
 import Data.IntervalMap.FingerTree (Interval(..))
 import Data.Map qualified as M
 
-data Music v a
-  = Everywhere a
-  | Voices (MonoidalMap v (Voice a))
+data Polyphony v a
+  = Pure a
+  | Polyphonic (MonoidalMap v (Voice a))
   deriving stock (Eq, Ord, Show, Functor, Foldable, Traversable, Generic)
 
-instance Ord v => Applicative (Music v) where
-  pure = Everywhere
-  liftA2 f (Everywhere a) (Everywhere b) = Everywhere $ f a b
-  liftA2 f (Voices a) (Everywhere b) = Voices $ fmap (fmap $ flip f b) a
-  liftA2 f (Everywhere a) (Voices b) = Voices $ fmap (fmap $ f a) b
-  liftA2 f (Voices a) (Voices b) =
-    Voices $
+instance Ord v => Applicative (Polyphony v) where
+  pure = Pure
+  liftA2 f (Pure a) (Pure b) = Pure $ f a b
+  liftA2 f (Polyphonic a) (Pure b) = Polyphonic $ fmap (fmap $ flip f b) a
+  liftA2 f (Pure a) (Polyphonic b) = Polyphonic $ fmap (fmap $ f a) b
+  liftA2 f (Polyphonic a) (Polyphonic b) =
+    Polyphonic $
       MM.mergeWithKey
         (const $ fmap pure . liftA2 f)
         (const MM.empty)
@@ -31,34 +31,34 @@ instance Ord v => Applicative (Music v) where
         a
         b
 
-instance (Ord v, Semigroup a) => Semigroup (Music v a) where
-  Everywhere a <> Everywhere b = Everywhere (a <> b)
-  Voices a <> Everywhere b = Voices $ fmap (fmap (<> b)) a
-  Everywhere a <> Voices b = Voices $ fmap (fmap (a <>)) b
-  Voices a <> Voices b = Voices $ a <> b
+instance (Ord v, Semigroup a) => Semigroup (Polyphony v a) where
+  Pure a <> Pure b = Pure (a <> b)
+  Polyphonic a <> Pure b = Polyphonic $ fmap (fmap (<> b)) a
+  Pure a <> Polyphonic b = Polyphonic $ fmap (fmap (a <>)) b
+  Polyphonic a <> Polyphonic b = Polyphonic $ a <> b
 
-instance (Ord v, Monoid a) => Monoid (Music v a) where
-  mempty = Everywhere mempty
+instance (Ord v, Monoid a) => Monoid (Polyphony v a) where
+  mempty = Pure mempty
 
 
 data Voice a
   = Voice (Sum Rational) (SF Rational a)
-  | Always a
+  | Drone a
   deriving stock (Eq, Ord, Show, Functor, Foldable, Traversable, Generic)
   deriving (Semigroup, Monoid) via Ap Voice a
 
 instance Applicative Voice where
-  pure = Always
-  liftA2 f (Always a) (Always b) = Always $ f a b
-  liftA2 f (Voice d1 a) (Always b) = Voice d1 $ fmap (flip f b) a
-  liftA2 f (Always a) (Voice d2 b) = Voice d2 $ fmap (f a) b
+  pure = Drone
+  liftA2 f (Drone a) (Drone b) = Drone $ f a b
+  liftA2 f (Voice d1 a) (Drone b) = Voice d1 $ fmap (flip f b) a
+  liftA2 f (Drone a) (Voice d2 b) = Voice d2 $ fmap (f a) b
   liftA2 f (Voice d1 a) (Voice d2 b) =
     Voice (d1 <> d2) $ liftA2 f a b
 
 
 delay :: Rational -> Voice a -> Voice a
 delay o (Voice d (SF m e)) = Voice d $ SF (M.mapKeys (+ o) m) e
-delay _ (Always a) = Always a
+delay _ (Drone a) = Drone a
 
 
 -- | The identity on '(##)'.
@@ -69,7 +69,7 @@ silent = rest 0
 -- | Tile product (eg "play this before that")
 (##) :: Monoid a => Voice a -> Voice a -> Voice a
 (##) v1@(Voice d _) = liftA2 (<>) v1 . delay (getSum d)
-(##) (Always a) = fmap (a <>)
+(##) (Drone a) = fmap (a <>)
 infixr 6 ##
 
 
@@ -102,10 +102,10 @@ flatten (Voice _ (SF m _)) = do
   ((lo, _), (hi, ma)) <- zip ((0, error "impossible") : m') m'
   guard $ lo /= hi
   pure (Interval lo hi, ma)
-flatten Always {} = mempty
+flatten Drone {} = mempty
 
 
 sample :: Rational -> Voice a -> a
 sample t (Voice _ sf) = sf ! t
-sample _ (Always a) = a
+sample _ (Drone a) = a
 
