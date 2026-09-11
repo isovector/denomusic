61075df225f3f0a78d87860f158dcca56f9ad4ef
Sandy Maguire <sandy@sandymaguire.me>
2026-09-10 22:37:05 -0700

    Generalize SF output

--- src/FRP.hs
+++ src/FRP.hs
@@ -15,8 +15,6 @@ import Data.Maybe
 import Data.Monoid
 import Data.Ratio (denominator)
 import Data.Semigroup qualified as S
-import Data.Set (Set)
-import Data.Set qualified as S
 import GHC.Generics
 import Prelude hiding (id, (.))
 
@@ -60,24 +58,21 @@ instance Semigroup a => Semigroup (Event a) where
 instance Semigroup a => Monoid (Event a) where
   mempty = NoEvent
 
-
-data Music = Music {}
-  deriving stock (Eq, Ord, Show)
-
-data Signal a = Signal
+data Signal m a = Signal
   { clock  :: Clock
-  , sample :: Time -> Writer (Set Music) a
+  , sample :: Time -> Writer [m] a
   }
   deriving stock (Functor, Generic, Generic1)
-  deriving Applicative via Generically1 Signal
+  deriving Applicative via Generically1 (Signal m)
 
-newtype SF a b = SF { runSF :: Signal a -> Signal b }
+newtype SF m a b = SF { runSF :: Signal m a -> Signal m b }
+  deriving (Functor, Applicative) via WrappedArrow (SF m) a
 
-instance Category SF where
+instance Category (SF m) where
   id = SF id
   SF g . SF f = SF (g . f)
 
-instance Arrow SF where
+instance Arrow (SF m) where
   arr = SF . fmap
   SF f *** SF g = SF $ \sb -> liftA2 (,) (f $ fmap fst sb) (g $ fmap snd sb)
   -- potential improvement?
@@ -92,7 +87,7 @@ instance Arrow SF where
   --     pure (a', b')
 
 
-sf :: Clock -> (Time -> a -> b) -> SF a b
+sf :: Clock -> (Time -> a -> b) -> SF m a b
 sf clk' f = SF $ \(Signal clk s) ->
   Signal (clk <> clk') $ \t -> do
     a <- s t
@@ -100,36 +95,36 @@ sf clk' f = SF $ \(Signal clk s) ->
 
 
 
-every :: Time -> a -> SF x (Event a)
+every :: Time -> a -> SF m x (Event a)
 every dur a = sf (Clock $ iterate (+ dur) 0) $ \t _ ->
   case denominator (t / dur) == 1 of
     True -> Event a
     False -> NoEvent
 
-now :: a -> SF x (Event a)
+now :: a -> SF m x (Event a)
 now a = sf (Clock [0]) $ \t _ ->
   case t == 0 of
     True -> Event a
     False -> NoEvent
 
-emit :: SF (Event Music) ()
-emit = arr (fmap S.singleton) >>> emitMany
+emit :: SF m (Event m) ()
+emit = arr (fmap pure) >>> emitMany
 
-emitMany :: SF (Event (Set Music)) ()
+emitMany :: SF m (Event [m]) ()
 emitMany = SF $ \(Signal clk s) ->
   Signal clk $ \t -> do
     s t >>= \case
       Event x -> tell x
       NoEvent -> pure ()
 
-play :: SF (Event (Time, Music)) (Event ())
+play :: SF m (Event (Time, m)) (Event ())
 play = proc e -> do
   emit -< fmap snd e
   arr void <<< delaySF -< e
 
 
 -- TODO(sandy): unwise?
-censor :: SF a (Event (Set Music))
+censor :: SF m a (Event [m])
 censor = SF $ \(Signal clk s) -> Signal clk $ \t -> do
   let (_, mus) = runWriter $ s t
   pure $
@@ -137,13 +132,13 @@ censor = SF $ \(Signal clk s) -> Signal clk $ \t -> do
       True -> NoEvent
       False -> Event mus
 
-playBefore :: SF (Event (Time, Music)) (Event ())
+playBefore :: SF m (Event (Time, m)) (Event ())
 playBefore = proc evs -> do
   delayed <- delaySF -< fmap (negate *** id) evs
   emit -< delayed
   returnA -< void delayed
 
-delaySF :: SF (Event (Time, a)) (Event a)
+delaySF :: SF m (Event (Time, a)) (Event a)
 delaySF = SF $ \(Signal clk s) -> do
   let sampled = do
         t <- getClock clk
@@ -157,18 +152,18 @@ delaySF = SF $ \(Signal clk s) -> do
 foldEvs :: (Time -> Writer x (Event a)) -> [Time] -> [(Time, a)]
 foldEvs s ts = mapMaybe sequenceA $ zip ts $ fmap (eventToMaybe . fst . runWriter . s) ts
 
-hold :: a -> SF (Event a) a
+hold :: a -> SF m (Event a) a
 hold a0 = SF $ \(Signal clk s) -> do
   let xs = foldEvs s $ getClock clk
   Signal clk $ \t -> do
     _ <- s t
     pure $ S.getLast $ S.sconcat $ coerce $ a0 :| fmap snd (takeWhile ((<= t) . fst) xs)
 
-delay :: Time -> SF a a
+delay :: Time -> SF m a a
 delay dt = SF $ \(Signal clk s) ->
   Signal (coerce (fmap @[] (+ dt)) clk) $ s . subtract dt
 
-time :: SF x Time
+time :: SF m x Time
 time = sf mempty const
 
 
@@ -178,7 +173,7 @@ data Observation a = Observation
   }
   deriving stock (Eq, Ord, Show, Functor)
 
-observe :: SF () a -> [Observation (Set Music, a)]
+observe :: SF m () a -> [Observation ([m], a)]
 observe (SF f) = do
   let Signal (Clock clk) s = f $ pure ()
   t <- clk
@@ -191,22 +186,22 @@ gate False _ = NoEvent
 gate True e = e
 
 
-afterNext :: SF (Event what, Event when) (Event (what, when))
+afterNext :: SF m (Event what, Event when) (Event (what, when))
 afterNext = proc (ewhat, ewhen) -> do
   ewhen' <- delay 0.000000001 -< ewhen
   mwhat <- hold Nothing -< asum [fmap Just ewhat, Nothing <$ ewhen']
   returnA -< ewhen >>= \when -> MkEvent $ fmap (, when) mwhat
 
-notYet :: SF (Event a) (Event a)
+notYet :: SF m (Event a) (Event a)
 notYet = sf (Clock [0]) $ \t a ->
   case t <= 0 of
     True -> NoEvent
     False -> a
 
-once :: SF (Event a) (Event a)
+once :: SF m (Event a) (Event a)
 once = takeEvents 1
 
-takeEvents :: Int -> SF (Event a) (Event a)
+takeEvents :: Int -> SF m (Event a) (Event a)
 takeEvents n = proc ev -> do
   ev' <- accum (0, NoEvent)
       -< ev <&> \a -> \(n', _) ->
@@ -214,14 +209,14 @@ takeEvents n = proc ev -> do
   returnA -< ev' >>= snd
 
 
-dropEvents :: Int -> SF (Event a) (Event a)
+dropEvents :: Int -> SF m (Event a) (Event a)
 dropEvents n = proc ev -> do
   ev' <- accum (n, NoEvent)
       -< ev <&> \a -> \(n', _) ->
           bool (n', Event a) (n' - 1, NoEvent) $ n' > 0
   returnA -< ev' >>= snd
 
-accum :: a -> SF (Event (a -> a)) (Event a)
+accum :: a -> SF m (Event (a -> a)) (Event a)
 accum a0 = SF $ \(Signal clk s) -> do
   let xs = scanl (\(_, a) (t, f) -> (t, f a)) (0, a0) $ foldEvs s $ getClock clk
   Signal clk $ \t -> do
@@ -230,7 +225,7 @@ accum a0 = SF $ \(Signal clk s) -> do
     pure $ (S.getLast $ S.sconcat $ coerce $ a0 :| fmap snd (takeWhile ((<= t) . fst) xs)) <$ ev
 
 
-onlyEvery :: Int -> SF (Event a) (Event a)
+onlyEvery :: Int -> SF m (Event a) (Event a)
 onlyEvery n = proc ev -> do
   x <- hold 0 <<< accum 0 -< (+1) <$ ev
   returnA -< bool NoEvent ev $ mod x n == 0
@@ -241,10 +236,10 @@ onlyEvery n = proc ev -> do
 main :: IO ()
 main = print $ take 10 $ filter (not . null . fst . o_output) $ takeWhile ((<= 100) . o_time) $ observe $ test
 
-test :: SF () ()
+test :: SF Int () ()
 test = proc _ -> do
   t <- dropEvents 4 <<< every 1 () -< ()
-  emit -< Music <$ t
+  emit -< 5 <$ t
   returnA -< ()
 
 
