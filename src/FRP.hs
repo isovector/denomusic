30b732d1cdef83aa9b5566ae49a06de111ca368d
Sandy Maguire <sandy@sandymaguire.me>
2026-09-23 10:38:03 -0700

    Switching

--- src/FRP.hs
+++ src/FRP.hs
@@ -8,14 +8,13 @@ module FRP
   , Interval(..)
   ) where
 
+import Control.Monad.Cont
 import Control.Applicative
 import Control.Arrow
 import Control.Category
 import Control.Exception (evaluate)
-import Control.Monad (join)
-import Control.Monad.Writer (runWriter, tell, mapWriter)
+import Control.Monad (join, (<=<))
 import Data.Align
-import Data.Bifunctor (bimap)
 import Data.Bool
 import Data.Coerce
 import Data.Either (partitionEithers)
@@ -24,7 +23,6 @@ import Data.List.NonEmpty (NonEmpty(..))
 import Data.Maybe
 import Data.Monoid
 import Data.Ratio
-import Data.Semigroup qualified as S
 import Data.Set (Set)
 import Data.Set qualified as S
 import Data.These
@@ -74,26 +72,26 @@ stretch r = SF $ \(Signal clk s) ->
 now :: a -> SF x (Event a)
 now = at 0
 
--- -- | Observe whether a computation would diverge, and if so, return 'Nothing'
--- -- instead. This can be used to guard otherwise-sketchy combinators which need
--- -- to fold over infinite event streams.
--- --
--- -- This is impolemented by terminating after 10ms of trying.
--- terminating :: a -> Maybe a
--- terminating = unsafePerformIO . timeout 10_000 . evaluate
+-- | Observe whether a computation would diverge, and if so, return 'Nothing'
+-- instead. This can be used to guard otherwise-sketchy combinators which need
+-- to fold over infinite event streams.
+--
+-- This is implemented by terminating after 10ms of trying.
+terminating :: a -> Maybe a
+terminating = unsafePerformIO . timeout 10_000 . evaluate
 
--- -- TODO(sandy): What would fswitch do? Run the first SF until the event in the
--- -- second would trigger?
--- switch :: SF a (b, Event c) -> (c -> SF a b) -> SF a b
--- switch (SF f) k = SF $ \sig0@Signal{} -> do
---   let sig1 = f sig0
---       sig1b = fmap fst sig1
---   case listToMaybe $ signalEvs $ fmap snd sig1 of
---     Nothing -> sig1b
---     Just (t0, c) -> do
---       let sig2 = runSF (offset t0 <<< k c <<< offset (- t0)) sig0
---       Signal (clock sig1 <> clock sig2) $ \t ->
---         flip sample t $ bool sig1b sig2 $ t >= t0
+-- TODO(sandy): What would fswitch do? Run the first SF until the event in the
+-- second would trigger?
+switch :: SF a (b, Event c) -> (c -> SF a b) -> SF a b
+switch (SF f) k = SF $ \sig0@Signal{} -> do
+  let sig1 = f sig0
+      sig1b = fmap fst sig1
+  case (terminating <=< listToMaybe) $ mapMaybe sequenceA $ signalEvs $ fmap snd sig1 of
+    Nothing -> sig1b
+    Just (t0, c) -> do
+      let sig2 = runSF (offset t0 <<< k c <<< offset (- t0)) sig0
+      Signal (clock sig1 <> clock sig2) $ \t ->
+        flip sample t $ bool sig1b sig2 $ t >= t0
 
 
 -- move :: SF (Event (Time, a)) (Event a)
@@ -212,4 +210,37 @@ onlyEvery :: Int -> SF (Event a) (Event a)
 onlyEvery n = proc ev -> do
   x <- hold 0 <<< accum 0 -< (+1) <$ ev
   returnA -< bool NoEvent ev $ mod x n == 0
+
+
+newtype Seq i o a = Seq
+  { unSeq :: Cont (SF i o) a
+  }
+  deriving newtype (Functor, Applicative, Monad)
+
+toSeq :: SF i (o, Event a) -> Seq i o a
+toSeq = Seq . cont . switch
+
+switchSeq :: Seq i o a -> (a -> SF i o) -> SF i o
+switchSeq = runCont . unSeq
+
+getSeq :: Seq i (Event o) a -> SF i (Event o)
+getSeq = flip switchSeq $ const $ arr $ const NoEvent
+
+rest :: Time -> Seq i (Event a) ()
+rest t = toSeq $ proc _ -> do
+  e <- at t () -< ()
+  returnA -< (NoEvent, e)
+
+pulse :: a -> Seq i (Event a) ()
+pulse a = toSeq $ proc _ -> do
+  n <- now a -< ()
+  returnA -< (n, void n)
+
+hit :: Time -> a -> Seq i (Event a) ()
+hit t a = do
+  pulse a
+  rest t
+
+beat :: Time -> Priority -> Seq i (Event Beat) ()
+beat t p = hit t $ Beat t p
 
